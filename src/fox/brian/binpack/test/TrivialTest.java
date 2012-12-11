1e1e25a4a0a9722e42a18431dcf3ddfc719e0b1c
Brian Fox <brianfox@chemcca8.ucsd.edu>
2012-12-11 15:13:02 -0800

    Added test case.

---
+++ src/fox/brian/binpack/test/TrivialTest.java
@@ -0,0 +1,14 @@
+package com.fox.brian.binpack.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TrivialTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

16ac2d81ca8a972c7a1926ddba038c4400ae1720
Brian Fox <brianfox@chemcca8.ucsd.edu>
2012-12-11 15:12:19 -0800

    Added documentation.

--- src/fox/brian/binpack/porthacks/outInt.java
+++ src/fox/brian/binpack/porthacks/outInt.java
@@ -1,5 +1,14 @@
 package com.fox.brian.binpack.porthacks;
 
+/**
+ * A quick and dirty way to pass input/output integer parameters to 
+ * functions.  This accommodates C++ constructs like:
+ * 
+ * void myFunc(int &myreturn) {
+ *     *myreturn = 1;
+ * }
+ * 
+ */
 public class outInt {
 	public int val;
 }

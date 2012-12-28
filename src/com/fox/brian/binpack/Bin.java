ecbe3d9a48b5775d25953567111ebd8c7b573597
Brian Fox <genkuro@gmail.com>
2012-12-28 12:10:00 -0800

    Simple generic container for bin packing.

---
+++ src/com/fox/brian/binpack/Bin.java
@@ -0,0 +1,55 @@
+package com.fox.brian.binpack;
+
+public class Bin<T> {
+
+	private T object;
+	private float x;
+	private float y;
+	private float width;
+	private float height;
+	private String summary;
+	
+	public Bin(T object, float width, float height, String summary) {
+		this.object = object;
+		this.height = height;
+		this.width = width;
+		this.x = -1;
+		this.y = -1;
+		this.summary = summary;
+	}
+	
+	public float getX() {
+		return x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public float getWidth() {
+		return width;
+	}
+
+	public float getHeight() {
+		return height;
+	}
+
+	public T getObject() {
+		return object;
+	}
+
+	public void setLocation(float x, float y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public Object toSummary() {
+		return summary;
+	}
+
+	public void reset() {
+		this.x = this.y = 0;
+		
+	}
+
+}
